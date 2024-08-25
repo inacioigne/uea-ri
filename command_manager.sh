045cd26ab60571aa6e203a3a8ebe35e57825dbf2
Inácio Oliveira <inacioigne@gmail.com>
2024-08-25 11:14:58 -0400

    command manager

---
+++ command_manager.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Função para mostrar o menu
+api_reacreate() {
+    echo "Recriando API:"
+}
+
+api_logs() {
+    echo "Logs Api"
+    docker compose -f ./docker-compose.yml logs api
+}
+
+
+if [ -z "$1" ]; then
+    echo "Erro: Nenhuma função fornecida."
+    exit 1
+fi
+
+funcao=$1
+
+if declare -f "$funcao" > /dev/null; then
+    $funcao
+else
+    echo "Erro: A função '$funcao' não existe"
+    exit 1
+fi

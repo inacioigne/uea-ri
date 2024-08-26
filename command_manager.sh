0fe9fe0bbf081be14373f3a67b6969ab2191c885
Inácio Oliveira <inacioigne@gmail.com>
2024-08-26 10:18:59 -0400

    command manager

--- command_manager.sh
+++ command_manager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Função para mostrar o menu
-api_reacreate() {
+api_recreate() {
     echo "Recriando API:"
     docker compose -f ./docker-compose.yml up -d --force-recreate api
 }
@@ -9,6 +9,11 @@ api_reacreate() {
 api_logs() {
     echo "Logs Api"
     docker compose -f ./docker-compose.yml logs api
+}
+
+ui_recreate() {
+    echo "Recriando Frontend:"
+    docker compose -f ./docker-compose.yml up -d --force-recreate ui
 }
 
 
